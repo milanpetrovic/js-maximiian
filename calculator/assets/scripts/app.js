15060aae1ab1059744ae816cb6cba0ba16f8c273
milanpetrovic <milanmushin@gmail.com>
2020-10-21 20:32:57 +0200

    More on strings

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -3,6 +3,6 @@ let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 2 - 4;
 
-let calculationDescription = '(' + defaultResult + ' + 10) * 2 - 4';
+let calculationDescription = `(${defaultResult} + 10) * 2 - 4`;
 
 outputResult(currentResult, calculationDescription);
