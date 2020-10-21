0652e0eb2f4d5dd4ead6cbfb5de818ead5d58f45
milanpetrovic <milanmushin@gmail.com>
2020-10-21 16:51:09 +0200

    Using constants

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -1,7 +1,8 @@
-let currentResult = 0;
+const defaultResult = 0;
+let currentResult = defaultResult;
 
 currentResult = (currentResult + 10) * 2 - 4;
 
-let calculationDescription = '(0 + 10) * 2 - 4';
+let calculationDescription = '(' + defaultResult + ' + 10) * 2 - 4';
 
 outputResult(currentResult, calculationDescription);
