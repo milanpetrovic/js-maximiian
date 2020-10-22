3118d4088f98a55853a11e7ab5951723506762de
milanpetrovic <milanmushin@gmail.com>
2020-10-22 03:13:07 +0200

    Adding a custom function

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -1,6 +1,13 @@
 const defaultResult = 0;
 let currentResult = defaultResult;
 
+function add(num1, num2) {
+    const result = num1 + num2;
+    alert(`Result is ${result}`);
+}
+
+add(20, 19);
+
 currentResult = (currentResult + 10) * 2 - 4;
 
 let calculationDescription = `(${defaultResult} + 10) * 2 - 4`;
