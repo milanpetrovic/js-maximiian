b22056bd4b87a50ecf1a2b1165a4fa42842bca12
milanpetrovic <milanmushin@gmail.com>
2020-10-23 14:17:37 +0200

    Connecting all buttons to functions

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -5,11 +5,40 @@ function getUserNumberInput() {
     return parseInt(userInput.value);
 }
 
-function add() {
-    const enteredNumber = getUserNumberInput();
-    const calcDescription = `${currentResult} + ${enteredNumber}`;
-    currentResult = currentResult + enteredNumber;
+function createAndWriteOutput(operator, resultBeforeCalc, enteredNumber) {
+    const calcDescription = `${resultBeforeCalc} ${operator} ${enteredNumber}`;
     outputResult(currentResult, calcDescription);
 }
 
+function add() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult + enteredNumber;
+    createAndWriteOutput('+', initialResult, enteredNumber);
+}
+
+function subtract() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult - enteredNumber;
+    createAndWriteOutput('-', initialResult, enteredNumber);
+}
+
+function multiply() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult * enteredNumber;
+    createAndWriteOutput('*', initialResult, enteredNumber);
+}
+
+function divide() {
+    const enteredNumber = getUserNumberInput();
+    const initialResult = currentResult;
+    currentResult = currentResult / enteredNumber;
+    createAndWriteOutput('/', initialResult, enteredNumber);
+}
+
 addBtn.addEventListener('click', add);
+subtractBtn.addEventListener('click', subtract);
+multiplyBtn.addEventListener('click', multiply);
+divideBtn.addEventListener('click', divide);
