ddf147519e16c3a2e11206227ea060e5003f52d4
milanpetrovic <milanmushin@gmail.com>
2020-11-11 15:02:06 +0100

    Adding bind() to the Calculator Project

--- calculator/assets/scripts/app.js
+++ calculator/assets/scripts/app.js
@@ -24,58 +24,28 @@ function writeToLog(operation, prevResult, newNumber, newResult) {
     console.log(logEntries);
 }
 
-// Function that handles all calculations based on provided type
-function calculateResult(calculationType) {
+function calculate(operation) {
     const enteredNumber = getUserNumberInput();
-
-    if (
-        calculationType !== 'ADD' &&
-        calculationType !== 'SUBTRACT' && 
-        calculationType !== 'MULTIPLY' && 
-        calculationType !== 'DIVIDE' ||
-        !enteredNumber // falsy, 0 value
-    ) {
-        return;
-    }
-    
     const initialResult = currentResult;
-    let mathOperator;
-
-    if (calculationType === 'ADD') {
+    let operator;
+    if (operation === 'ADD') {
         currentResult += enteredNumber;
-        mathOperator = '+';
-    } else if (calculationType === 'SUBTRACT') {
+        operator = '+';
+    } else if (operation === 'SUBTRACT') {
         currentResult -= enteredNumber;
-        mathOperator = '-';
-    } else if (calculationType === 'MULTIPLY') {
+        operator = '-';
+    } else if (operation === 'MULTIPLY') {
         currentResult *= enteredNumber;
-        mathOperator = '*';
-    } else if(calculationType === 'DIVIDE') {
+        operator = '*';
+    } else {
         currentResult /= enteredNumber;
-        mathOperator = '/';
+        operator = '/';
     }
-
-    createAndWriteOutput(mathOperator, initialResult, enteredNumber);
-    writeToLog(calculationType, initialResult, enteredNumber, currentResult);
+    createAndWriteOutput(operator, initialResult, enteredNumber);
+    writeToLog(operation, initialResult, enteredNumber, currentResult);
 }
 
-function add() {
-    calculateResult('ADD');
-}
-
-function subtract() {
-    calculateResult('SUBTRACT');
-}
-
-function multiply() {
-    calculateResult('MULTIPLY');
-}
-
-function divide() {
-    calculateResult('DIVIDE');
-}
-
-addBtn.addEventListener('click', add);
-subtractBtn.addEventListener('click', subtract);
-multiplyBtn.addEventListener('click', multiply);
-divideBtn.addEventListener('click', divide);
+addBtn.addEventListener('click', calculate.bind(this, 'ADD'));
+subtractBtn.addEventListener('click', calculate.bind(this, 'SUBTRACT'));
+multiplyBtn.addEventListener('click', calculate.bind(this, 'MULTIPLY'));
+divideBtn.addEventListener('click', calculate.bind(this, 'DIVIDE'));
