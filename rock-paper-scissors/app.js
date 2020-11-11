0d1c70f76dd00932e6d762797a56a4ac2eed5873
milanpetrovic <milanmushin@gmail.com>
2020-11-11 11:49:06 +0100

    Introducing arrow functions

--- rock-paper-scissors/app.js
+++ rock-paper-scissors/app.js
@@ -10,7 +10,7 @@ const RESULT_COMPUTER_WINS = 'COMPUTER WINS';
 
 let gameIsRunning = false;
 
-const getPlayerChoice = function() {
+const getPlayerChoice = () => {
     const selection = prompt(`${ROCK}, ${PAPER}, or ${SCISSORS}`, '').toUpperCase();
 
     if (selection != ROCK && selection != PAPER && selection != SCISSORS) {
@@ -21,7 +21,7 @@ const getPlayerChoice = function() {
     return selection;
 };
 
-const getComputerChoice = function() {
+const getComputerChoice = () => {
     const randomValue = Math.random();
 
     if (randomValue < 0.34) {
@@ -33,21 +33,16 @@ const getComputerChoice = function() {
     }
 };
 
-const getWinner = function(cChoice, pChoice) {
-    if (cChoice === pChoice) {
-        return RESULT_DRAW;
-    } else if (
-        cChoice === ROCK && pChoice === PAPER ||
-        cChoice === PAPER && pChoice === SCISSORS ||
-        cChoice === SCISSORS && pChoice === ROCK
-        ) {
-        return RESULT_PLAYER_WINS;
-    } else {
-        return RESULT_COMPUTER_WINS;
-    }
-};
+const getWinner = (cChoice, pChoice) => 
+    cChoice === pChoice 
+    ? RESULT_DRAW 
+    : cChoice === ROCK && pChoice === PAPER ||
+      cChoice === PAPER && pChoice === SCISSORS ||
+      cChoice === SCISSORS && pChoice === ROCK
+    ? RESULT_PLAYER_WINS 
+    : RESULT_COMPUTER_WINS;
 
-startGameBtn.addEventListener('click', function() {
+startGameBtn.addEventListener('click', () => {
     if (gameIsRunning) {
         return;
     }
