2eba6d280174d6aa046dcc01ab42315d039657c1
milanpetrovic <milanmushin@gmail.com>
2020-11-12 16:58:19 +0100

    Oppening a modal by changing CSS classes

--- favorite-movies-dom/assets/scripts/app.js
+++ favorite-movies-dom/assets/scripts/app.js
@@ -1,2 +1,8 @@
 const addMovieModal = document.getElementById('add-modal');
 const addBtnTop = document.querySelector('header button');
+
+const toggleMovieModal = (modal) => {
+    addMovieModal.classList.toggle('visible');
+};
+
+addBtnTop.addEventListener('click', toggleMovieModal);
