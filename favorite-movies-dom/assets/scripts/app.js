03bdc04bb433b7665cab7bb6f52a4a8e1d6ba848
milanpetrovic <milanmushin@gmail.com>
2020-11-12 17:14:59 +0100

    Controlling the backdrop

--- favorite-movies-dom/assets/scripts/app.js
+++ favorite-movies-dom/assets/scripts/app.js
@@ -1,8 +1,25 @@
 const addMovieModal = document.getElementById('add-modal');
 const addBtnTop = document.querySelector('header button');
+const backdrop = document.getElementById('backdrop');
+const cancelAddBtn = addMovieModal.querySelector('.btn--passive');
 
-const toggleMovieModal = (modal) => {
+const toggleBackdrop = () => {
+    backdrop.classList.toggle('visible');
+};
+
+const toggleMovieModal = () => {
     addMovieModal.classList.toggle('visible');
+    toggleBackdrop();
+};
+
+const cancelAddMovie = () => {
+    toggleMovieModal();
+};
+
+const backdropClickHandler = () => {
+    toggleMovieModal();
 };
 
 addBtnTop.addEventListener('click', toggleMovieModal);
+cancelAddBtn.addEventListener('click', cancelAddMovie);
+backdrop.addEventListener('click', backdropClickHandler);
